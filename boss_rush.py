e9a0c225e43f32784d42b71f4cf15a5783f66a3c
alejandronava562 <navaalejandro562@gmail.com>
2025-11-07 17:57:33 -0800

    JSON WIP

--- boss_rush.py
+++ boss_rush.py
@@ -29,13 +29,74 @@ class Robert:
 STATE: Dict[str, Any] = {
     "player": BOBBY(),
     "bosses" : [
-        Robert(name = "The Landfill Lord", category = "landfills, trash").__dict__,
+        Robert(name = "The Landfill Lord", category = "").__dict__,
         Robert(name = "Carbon King", category = "carbon footprint, pollution, global warming").__dict__,
         Robert(name = "Mr. Incinerator", category = "waste burning,  pollution").__dict__,
-        Robert(name = "The Lorax Jr", category = "Tree cutter").__dict__
-    ]
+        Robert(name = "The Lorax Jr", category = "Habitats and Ecosystem").__dict__,
+        Robert(name = "Tree Slayer", category = "Tree cutter").__dict__, 
+    ],
+    "log": []
 }
+
+# =============================== #
+# ---------- Prompting ---------- #
+# =============================== #
+SYSTEM = (
+    ""
+)
+
+def build_scene_prompt(boss: Robert, player: BOBBY) -> str:
+    SCENE_PROMPT = f"""
+    Boss Name: {boss.name}
+    Boss Category: {boss.category}
+
+    Current Stats:
+    - Player HP: {player.hp}
+    - Boss HP: {boss.hp}
+
+    Stats:
+    - Player delta keys: hp
+    - Boss delta keys: hp
+
+    Write a new battle scene that fits the boss topic and
+    Scene Rules:
+    - 3 to 5 sentences
+    - Offer 4 choices
+
+    Return a strict JSON only. Template:
+    {{
+        "choices" : [
+        {{
+            "id: "A",
+            "text" : "choice text",
+            "delta_player": {"hp": "int in range (-10, 0)"}
+            "delta_boss": {"hp": "int in range (-10, 0)"}
+        }},
+        {{
+            "id: "B",
+            "text" : "choice text",
+            "delta_player": {"hp": "int in range (-10, 0)"}
+            "delta_boss": {"hp": "int in range (-10, 0)"}
+        }},
+        {{
+            "id: "C",
+            "text" : "choice text",
+            "delta_player": {"hp": "int in range (-10, 0)"}
+            "delta_boss": {"hp": "int in range (-10, 0)"}
+        }},
+        {{
+            "id: "D",
+            "text" : "choice text",
+            "delta_player": {"hp": "int in range (-10, 0)"}
+            "delta_boss": {"hp": "int in range (-10, 0)"}
+        }},
+        ]
+    }}
+    
+    """
+    return SCENE_PROMPT
+    
+
 
 print("Welcome to the eco-sustainible Boss Rush Game, in this game you will fight bosses that aren't eco-sustainible, and by answering sustainible questions right, you can damage the bosses.Good Luck!") 
 print(" Welcome to the bike-travelling Boss Escape Game, in this game you will run around and avoid the smashing car on a bike! You can escape! Good Luck!")
-    
